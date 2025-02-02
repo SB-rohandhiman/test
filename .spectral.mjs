106272d99283f3a60145d3b6131ea1a4af1fc44c
rohandhiman222@gmail.com <rohandhiman222@gmail.com>
2025-02-02 11:07:19 +0530

    Added 142 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-19840.stoplight-dev.com/api/v1/projects/cHJqOjc2Mjc/spectral.js?branch_slug=main&token=914f8294-c06c-4207-b7d3-5cc6b05d53e7";
+export default { extends: ruleset };
---
+++ docs/acceptor/getting-started.md
@@ -0,0 +1,8 @@
+---
+stoplight-id: dzwznxe8nvk3g
+---
+
+# Introduction
+
+This API allows Acceptors (Agents and Merchants) to perform operations and transactions on Skom It Bank master platform.
+
